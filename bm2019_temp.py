a9f4ff63704835304bdfbf32bb1e8af3d942da37
Mike Pesavento <mike@peztek.com>
2019-11-30 15:45:51 -0800

    plot of 24H daily average temperature

--- bm2019_temp.py
+++ bm2019_temp.py
@@ -8,6 +8,7 @@ https://mpesavento.github.io/phage_temperature_2019/
 """
 import os
 import sys
+import datetime
 from dataclasses import dataclass, field
 
 import pandas as pd
@@ -47,6 +48,35 @@ BLACK_ROCK_CITY_ELEVATION = 1100  # meters
 # smarter would be to check the data registry for max/min and fid the nearest order of 10
 TEMPERATURE_RANGE = (50, 110)
 
+LINE_OPACITY = 0.8
+
+LINE_COLORS = [
+    "#1f77b4",  # muted blue
+    "#ff7f0e",  # safety orange
+    "#2ca02c",  # cooked asparagus green
+    "#d62728",  # brick red
+    "#9467bd",  # muted purple
+    "#8c564b",  # chestnut brown
+    "#e377c2",  # raspberry yogurt pink
+    "#7f7f7f",  # middle gray
+    "#bcbd22",  # curry yellow-green
+    "#17becf",  # blue-teal
+]
+
+# ------------------------------------------------
+# utility methods
+
+def is_interactive():
+    # type: () -> bool
+    """Returns True if in an interactive session"""
+    # noinspection PyUnresolvedReferences
+    import __main__ as main_
+    if "pydevconsole" in sys.argv[0]:
+        return True
+    if not hasattr(main_, "__file__"):
+        return True
+    return False
+
 
 # ------------------------------------------------
 # data classes
@@ -66,6 +96,7 @@ class TemperatureSource:
     data_tz: str = PACIFIC_TZ
     display_tz: str = PACIFIC_TZ
     tags: list = field(default_factory=list)
+    color: str = LINE_COLORS[0]
     # data: pd.DataFrame=field(default_factory=_read_csv)
 
     @staticmethod
@@ -113,6 +144,10 @@ class TemperatureSource:
             raise ValueError("invalid temperature unit selected, unable to convert")
         self.data["temperature K"] = self.data[f"temperature C"] - 273.15
 
+    @property
+    def name(self):
+        return "{} - {}".format(self.recording_location, self.owner)
+
 
 # ------------------------------------------------
 # data registry
@@ -123,12 +158,14 @@ REGISTERED_DATA = [
         recording_location="shiftpod2",
         owner="mjp",
         tags=["shade", "swampcooler"],
+        color=LINE_COLORS[0],
     ),
     TemperatureSource(
         filename="burningman_2019_pnelson_shiftpod2_blastshield_swampcooler.csv",
         recording_location="shiftpod2",
         owner="pnelson",
         tags=["shade", "swampcooler", "blastshield"],
+        color=LINE_COLORS[1],
     ),
     TemperatureSource(
         filename="BurningMan2019 - Altitude Lounge 845&C.csv",
@@ -138,6 +175,8 @@ REGISTERED_DATA = [
         temp_col="Outdoor Temperature (F)",
         temp_units="F",
         tags=[],
+        color="#333333",
+
     ),
     TemperatureSource(
         filename="burningman_2019_myq_h12yurt.csv",
@@ -145,6 +184,7 @@ REGISTERED_DATA = [
         owner="myq",
         tags=["AC?"],
         data_tz=UTC_TZ,
+        color=LINE_COLORS[2],
     ),
     TemperatureSource(
         filename="burningman_2019_bunnie_shiftpod-clipped.csv",
@@ -152,6 +192,7 @@ REGISTERED_DATA = [
         owner="bunnie",
         tags=["AC"],
         data_tz=UTC_TZ,
+        color=LINE_COLORS[3],
     ),
 ]
 
@@ -163,6 +204,7 @@ def get_sun_transitions(
     start_date="2019-08-22",
     end_date="2019-09-03"
 ):
+    """Get BRC sunrise and sunset times between date range, date inclusive"""
     dates = pd.date_range(start_date, end_date, freq="D")
 
     a = Astral()
@@ -240,20 +282,30 @@ def get_source_data_trace(source, units="F"):
         x=source.data["datetime"],
         y=source.data[field],
         mode="lines",
-        name="{} - {}".format(source.recording_location, source.owner),
+        line=dict(color=source.color),
+        name=source.name,
+        opacity=LINE_OPACITY,
+
     )
     return trace
 
 
-def plot_temperatures(data_sources, units="F"):
+def plot_week_temperatures(data_sources: list, sun_df: pd.DataFrame, units="F"):
     if not hasattr(data_sources[0], "data"):
         load_data_files()
 
-    temp_traces = [get_source_data_trace(source) for source in data_sources]
-    night_traces = get_night_rect_traces(get_sun_transitions())
+    temperature_traces = [get_source_data_trace(source) for source in data_sources]
+    # # change the line color of the traces here
+    # for i, trace in enumerate(temperature_traces):
+    #     clr = LINE_COLORS[i] if "outdoors" not in trace.name else "black"
+    #     # width = 2 if "outdoors" not in trace.name else 3
+    #     width = 2
+    #     trace['line'] = dict(width=width, color=clr)
+
+    night_traces = get_night_rect_traces(sun_df)
 
     # order matters, we want the night traces below the temp traces
-    traces = night_traces + temp_traces
+    traces = night_traces + temperature_traces
 
     fig = go.Figure(
         data=traces,
@@ -265,25 +317,165 @@ def plot_temperatures(data_sources, units="F"):
             yaxis=dict(title=f"temperature {units}", range=TEMPERATURE_RANGE),
         ),
     )
-    out_filename = os.path.join(FIGURE_PATH, "phage_temperature_2019.html")
+    out_filename = os.path.join(FIGURE_PATH, "phage_temperature_{}_2019.html".format(units))
     plot(fig, filename=out_filename)
+    return fig
 
 
 def plot_temperature_distributions():
-    # TODO: create histograms of each dwelling. make outside gray, and plotted first
+    # TODO: create histograms of each dwelling. make outdoors gray, and plot it first
     pass
 
 
-def plot_period_averages():
-    pass
-    # TODO: do the average sunset to sunrise for each trace
-    # fix the sunrise-sunset times as the average over the week
+def ts_to_epoch_seconds(t) -> float:
+    """
+    Convert pandas Timestamp to epoch time in seconds
+    Can be a single timestamp or a Series of Timestamps
+    """
+    return t.astype(int) / 1e9
+
+
+def mean_time(ser: pd.Series, shift_timezone=True) -> datetime.time:
+    """
+    Find the average time over a series of Timestamps
+    EG, what is the average sunrise time
+    """
+    mean_epoch_time = ts_to_epoch_seconds(ser).mean()
+    mean_datetime = pd.to_datetime(mean_epoch_time, unit="s")
+    if shift_timezone:
+        # we only need to do this if the time series we get in is not TZ aware already
+        mean_datetime = mean_datetime.tz_localize(UTC_TZ).tz_convert(PACIFIC_TZ)
+    return mean_datetime.time()
+
+
+def plot_period_averages(data_sources: list, sun_df: pd.DataFrame):
+    """Create a plot of the average temperatures over a 24 hour period"""
+    daily_mean_df = extract_daily_means(data_sources)
+
+    traces = []
+
+    # There is a bug in here with using datetime.time as the x axis. Things
+    # don't line up as expected, so we need to have the first trace(s) have
+    # a full sampling of all timestamps from the data sources
+    # to be able to plot the night shading rects. Dunno why.
+    # To solve this, we just plot an invisible line with our target x axis
+    # before the night rects
+    traces.append(
+        go.Scatter(
+            x=daily_mean_df.index,
+            y=[60] * len(daily_mean_df.index),
+            mode="lines",
+            line=dict(width=0),
+            name="placeholder",
+            showlegend=False,
+        )
+    )
+
+    # shade the night time
+    # these traces need to be after the temperature traces, because indexing is weird for datetime.time
+    sunset = mean_time(sun_df.sunset)
+    sunrise = mean_time(sun_df.sunrise)
+    y_range = (60, 100)  # temperature range, in F
+    traces.extend([
+        # NOTE: the sunset/sunrise datetime.time needs to perfectly align with the times in the temperature traces
+        go.Scatter(
+            x=[
+                datetime.time(0, 0),
+                datetime.time(0, 0),
+                datetime.time(6, 15),
+                datetime.time(6, 15),
+                # sunrise,
+                # sunrise,
+            ],
+            y=[y_range[0], y_range[1], y_range[1], y_range[0]],
+            fill="toself",
+            fillcolor="grey",
+            mode="lines",
+            line=dict(width=0),
+            opacity=0.3,
+            showlegend=True,
+            name="night",
+        ),
+        go.Scatter(
+            x=[
+                # sunset,
+                # sunset,
+                datetime.time(19, 42),
+                datetime.time(19, 42),
+                datetime.time(23, 57),
+                datetime.time(23, 57),
+            ],
+            y=[y_range[0], y_range[1], y_range[1], y_range[0]],
+            fill="toself",
+            fillcolor="grey",
+            mode="lines",
+            line=dict(width=0),
+            opacity=0.3,
+            showlegend=False,
+            name="night",
+        ),
+    ])
+
+    # create traces for each of the source day averages
+    for i, source in enumerate(data_sources):
+        traces.append(
+            go.Scatter(
+                x=daily_mean_df.index,
+                y=daily_mean_df[source.name],
+                mode="lines",
+                line=dict(width=2, color=source.color),
+                name=source.name,
+                opacity=LINE_OPACITY,
+            )
+        )
+
+    fig = go.Figure(
+        data=traces,
+        layout=dict(
+            width=1200,
+            height=800,
+            title="Dwelling 24H average temperature - BRC 2019",
+            xaxis=dict(title="date"),
+            yaxis=dict(title=f"temperature (F)", range=y_range),
+        ),
+    )
+    out_filename = os.path.join(FIGURE_PATH, "phage_average_temperature_2019.html")
+    plot(fig, filename=out_filename)
+    return fig
+
+
+def extract_daily_means(data_sources: list) -> pd.DataFrame:
+    daily_mean = {}
+    for source in data_sources:
+        daily_mean[source.name] = daily_mean_for_source(source)
+    daily_mean_df = pd.concat(daily_mean, axis=1)
+    daily_mean_df.columns = daily_mean_df.columns.get_level_values(0)
+    return daily_mean_df
+
+
+def daily_mean_for_source(source) -> pd.Series:
+    # given the datasource, pull it as a series
+    tempf = source.data.set_index('datetime')['temperature F'].dropna()
+    # resample to make the timestamps consistent between data sources; fill with mean
+    tempf_df = tempf.resample("3T").bfill().to_frame().reset_index()
+
+    # add columns for date and time
+    tempf_df['date'] = tempf_df['datetime'].dt.date
+    tempf_df['time'] = tempf_df['datetime'].dt.time
+    daily_temp = tempf_df.groupby('time').mean()
+    return daily_temp
 
 
 def main():
     load_data_files(REGISTERED_DATA)
-    plot_temperatures(REGISTERED_DATA)
+    sun_df = get_sun_transitions()
+
+    # create plot of temperatures over all Burning Man recordings
+    plot_week_temperatures(REGISTERED_DATA, sun_df)
+
+    # create plot of average dwelling temperature over 24 hours
+    plot_period_averages(REGISTERED_DATA, sun_df)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__" and not is_interactive():
     sys.exit(main())
